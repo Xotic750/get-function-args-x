65605010bd0f870f494fd9fc2f99f72ca776fbb9
Graham Fairweather <xotic750@gmail.com>
2019-07-26 18:20:43 +0200

    :bookmark: v3.0.8
    Auto build

--- dist/get-function-args-x.js
+++ dist/get-function-args-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-25T22:56:20.454Z",
+  "date": "2019-07-26T16:20:37.045Z",
   "describe": "",
   "description": "Get the args of the function.",
   "file": "get-function-args-x.js",
   "hash": "fb5fb072bfe88a2b0b2a",
   "license": "MIT",
-  "version": "3.0.7"
+  "version": "3.0.8"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
