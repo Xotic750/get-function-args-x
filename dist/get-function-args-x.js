86a6421b854ab3ed81bc8c85e462d01c72abd0f5
Graham Fairweather <xotic750@gmail.com>
2019-08-04 17:50:51 +0200

    Auto build

--- dist/get-function-args-x.js
+++ dist/get-function-args-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-31T23:14:47.692Z",
+  "date": "2019-08-04T15:50:44.186Z",
   "describe": "",
   "description": "Get the args of the function.",
   "file": "get-function-args-x.js",
